61820be1707a028c323c87b10461b5c15e286210
Hugo Yvert <hugo.yvert@gmail.com>
2026-08-25 18:57:46 +0200

    ci: configuration workflow github actions

--- backend/routes/contenuRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contenuController = require('../controllers/contenuController');
-
-// Route GET pour récupérer les informations
-router.get('/', contenuController.getContenus);
-router.post('/', contenuController.create);
-
-module.exports = router;
